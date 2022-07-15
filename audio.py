4e22f566d7a0f92909375e739d6bf75626a551ad
Jamie <jitshel@gmail.com>
2022-07-14 21:00:57 -0700

    edited functions

--- audio.py
+++ audio.py
@@ -6,24 +6,20 @@
 
 import numpy as np
 from IPython.display import Audio
-from typing import Tuple
 from microphone import record_audio
-
+import librosa
 
 # In[7]:
 
 
-def mic_to_np_array(listen_time: float, file_path: str):
-    frames, sample_rate = record_audio(listen_time)
+def mic_to_np_array(listen_time):
+    frames, sampling_rate = record_audio(listen_time)
     samples = np.hstack([np.frombuffer(i, np.int16) for i in frames])
-    array_with_sample_rate = np.hstack((sample_rate, samples))
-    np.save(file_path, array_with_sample_rate)
+    return samples, sampling_rate
     
-def mp3_to_np_array(file_path: str) -> Tuple[np.ndarray, int]:
-    loaded_array = np.load(file_path)
-    sample_rate = loaded_array[0]
-    signal = loaded_array[1:]
-    return signal, sample_rate
+def mp3_to_np_array(file_path):
+    samples, sampling_rate = librosa.load(file_path, sr=44100, mono=True)
+    return samples, sampling_rate
 
 
 # In[ ]:
